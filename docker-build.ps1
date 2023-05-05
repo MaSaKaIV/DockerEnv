356fdd5afe5773b48ecfa4d1ee084850d7e24781
MaSaKaIV <masasuke674@gmail.com>
2023-05-05 14:31:29 +0900

    [Add] create python env by conda

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,7 +1,7 @@
 # !/bin/ps1
 
 docker-compose build
-# mysqlだけ先に起動しておく
-docker-compose run -d db
+# djangoイメージだけ先に起動しておく
+docker-compose run -d db --name=django
 # appコンテナの中に入る
 docker-compose run app /usr/bin/zsh
