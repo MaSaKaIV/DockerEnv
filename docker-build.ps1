010169e0b2a3431e781f6c93790ddbcb8548f7f0
MaSaKaIV <masasuke674@gmail.com>
2023-05-14 13:34:55 +0900

    [Fix] container name in docker-build.ps1

--- docker-build.ps1
+++ docker-build.ps1
@@ -2,6 +2,6 @@
 
 docker-compose build
 # djangoイメージだけ先に起動しておく
-docker-compose run -d --name=mysql_db db 
+docker-compose run -d --name=mysql-db db 
 # appコンテナの中に入る
 docker-compose run app /usr/bin/zsh
